bd0be3d38ab40244ea98d500ba95c190c14cd40f
Zeeshan <zeeshankalimkhan@gmail.com>
2024-02-14 01:09:44 +0530

    sorting and paging feature

--- MarketStore-API/controllers/productController.js
+++ MarketStore-API/controllers/productController.js
@@ -1,7 +1,15 @@
 const product = require('../models/product');
 
 const getAllProducts =async(req,res)=>{
-    const productList= await product.find({});
+    const {sort}=req.query;
+    let productList;
+    if(sort){
+        const sortList=sort.split(',').join(' ');
+        productList= await product.find({}).sort(sortList);
+    }
+    else
+        productList=await product.find({}) //.limit(10)
+
     res.status(200).json({
         data:productList,
         total:productList.length
@@ -9,7 +17,7 @@ const getAllProducts =async(req,res)=>{
 }
  
 const filterProduct=async(req,res)=>{
-    const {featured,company,name}=req.query;
+    const {featured,company,name,sort,fields,numericFilters}=req.query;
     const filterObject={}
 
     if(featured){
@@ -21,11 +29,57 @@ const filterProduct=async(req,res)=>{
         filterObject.name= {$regex:name,$options:'i'}
     }
 
-    const productList=await product.find(filterObject);
+    let productList= product.find(filterObject);
+    //only selecting the required fields of data
+    if(fields){
+        const fieldList=fields.split(',').join(' ')
+        productList=productList.select(fieldList);
+    }
+    //sorting result according to need
+    if(sort){
+        const sortList=sort.split(',').join(' ');
+        productList=productList.sort(sortList)
+    }
+
+    if(numericFilters){
+        const operatorMap={
+            '>':'&gt',
+            '>=':'&gte',
+            '=':'&eq',
+            '<':'&lt',
+            '<=':'&lte'
+        }
+
+        const regEx= /\b(<|>|>=|=|<|<=)\b/g
+        
+    }
+    //page implementation
+    const page=req.query.page;
+    const limit=5;
+    const skip=(page-1)*limit;
+
+    productList=await productList.skip(skip).limit(limit);
+
     if(productList.length!=0)
     res.status(201).json({data:productList,results:productList.length});
     else
         return res.status(404).json({msg:`No product found:`});
 }
 
-module.exports={getAllProducts,filterProduct};
+
+//pages functionality implementaion
+const getProductOverview=async (req,res)=>{
+    let {field}=req.query;
+    const page=req.query.page||1;
+    const skip=(page-1)*5;
+
+    field=field.split(',').join(' ')
+    const productList=await product.find({}).select(field).skip(skip).limit(5);
+
+    res.status(200).json({
+        data:productList,
+        total:productList.length
+    });
+}
+
+module.exports={getAllProducts,filterProduct,getProductOverview};
--- MarketStore-API/routes/productRoute.js
+++ MarketStore-API/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router=express.Router();
 
-const{getAllProducts,filterProduct}=require('../controllers/productController');
+const{getAllProducts,filterProduct,getProductOverview}=require('../controllers/productController');
 
 router.route('/products').get(getAllProducts);
 router.route('/products/filter').get(filterProduct);
+router.route('/products-overview').get(getProductOverview)
 
 module.exports=router;
